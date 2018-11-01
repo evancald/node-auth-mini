e62b2496c4438dc300d47d150d3cc29370a49d5e
Evan Caldwell <evancald@sas.upenn.edu>
2018-10-31 20:17:30 -0600

    finished login page

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const strategy = require(`${__dirname}/strategy.js`);
 
 const app = express();
+app.use( session({
+  secret: 'ahsuhdude',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+  done(null, {
+    id: user.id,
+    display: user.displayName,
+    nickname: user.nickname,
+    email: user.emails[0].value
+  });
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+})
+
+//Endpoints
+app.get('/login', passport.authenticate('auth0', {
+  successRedirect: '/me',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+app.get('/me', (req, res, next) => {
+  if ( !req.user ) {
+    res.redirect('/login');
+  } else {
+    res.status(200).send( JSON.stringify( req.user, null, 10 ) );
+  }
+});
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
